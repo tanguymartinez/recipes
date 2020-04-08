9bb1ec4a212f44515addd4afe56d340bcccbe359
Tanguy Martinez <tanguy.martinez@yahoo.com>
2020-04-08 17:39:34 +0000

    Adapt ListWheel usage

--- src/components/RunRecipe.vue
+++ src/components/RunRecipe.vue
@@ -1,7 +1,11 @@
 <template>
   <div ref="timer">
     <p>{{ current }}</p>
-    <list-wheel />
+    <list-wheel :items="items">
+      <template v-slot:default="{item}">
+        <p>{{item.value}}</p>
+      </template>
+    </list-wheel>
   </div>
 </template>
 
@@ -12,12 +16,16 @@ export default {
     return {
       id: null,
       start: 0,
-      current: 0
+      current: 0,
+      recipe: {},
+      items: []
     };
   },
   mounted: function() {
     this.start = Date.now();
     setInterval(this.timer, 100);
+    this.recipe = this.$store.getters.getRecipeById(this.$route.params.id);
+    this.items = this.recipe.instructions;
   },
   methods: {
     timer: function() {
@@ -32,9 +40,9 @@ export default {
 
 <style lang="sass" scoped>
 ul
-    list-style-type: none
-    padding: 0
+  list-style-type: none
+  padding: 0
 
 .widget
-    transition: all 250ms
+  transition: all 250ms
 </style>
