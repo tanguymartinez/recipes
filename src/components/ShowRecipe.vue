a290f07b64eab405f4f964e3fb53ad27d6b20ddf
Tanguy Martinez <tanguy.martinez@yahoo.com>
2020-04-12 10:41:56 +0000

    Change layout Change styling

--- src/components/ShowRecipe.vue
+++ src/components/ShowRecipe.vue
@@ -3,24 +3,23 @@
     <button-navigate value="Home" :to="{name: 'home'}" direction="left" />
     <h1>{{ recipe.name }}</h1>
     <p>{{ recipe.description }}</p>
-    <h2>Ingredients:</h2>
-    <ul>
-      <li v-for="(ingredient, idx) in recipe.ingredients" :key="ingredient + idx">
-        <div>
-          <p>{{ ingredient.value }}</p>
-          <p>x {{ ingredient.quantity + " " + ingredient.unit }}</p>
-        </div>
-      </li>
-    </ul>
-    <h2>Steps:</h2>
+    <div class="container-ingredients">
+      <h2>Ingredients</h2>
+      <ul>
+        <li v-for="(ingredient, idx) in recipe.ingredients" :key="ingredient + idx">
+          <span class="ingredient">{{ ingredient.value }}</span>
+          <span>x {{ ingredient.quantity + " " + ingredient.unit }}</span>
+        </li>
+      </ul>
+    </div>
+    <h2>Steps</h2>
     <ol>
       <li v-for="(instruction, idx) in recipe.instructions" :key="instruction + idx">
-        <div>
-          <p>{{ instruction.value }}</p>
-        </div>
+        <span>{{ idx + 1 }}</span>
+        <span>{{ instruction.value }}</span>
       </li>
     </ol>
-    <h2>Notes:</h2>
+    <h2>Notes</h2>
     <p>{{ recipe.notes }}</p>
     <button-navigate
       value="Edit"
@@ -58,19 +57,69 @@ h1
 ul, ol
   text-align: left
   li
-    >div
-      display: flex
-      justify-content: space-between
-      p
-        margin: 0
-        margin-bottom: 1rem
-        &:last-of-type
-          p
-            margin: 0
+    display: flex
+    justify-content: space-between
+    p
+      margin: 0
+      &:last-of-type
+        p
+          margin: 0
 h1
   text-align: center
 h2
   margin-top: 2rem
+  text-align: center
 .link.right
   margin-left: auto
+
+.container-ingredients
+  border: 5px solid #fcf451
+  border-radius: 10px
+  background-color: #2c3e50
+  box-shadow: 0 0 20px #30bfa3
+  color: white
+  h2
+    margin: 1rem 0
+    text-align: center
+  ul
+    list-style-type: none
+    padding: 0
+    margin: 0
+    display: grid
+    position: relative
+    grid-template: 1fr / 1fr 1fr
+    border-top: 2px solid white
+    li
+      padding: .5rem
+      border-bottom: 2px solid white
+      &:nth-child(2n+1)
+        border-right: 2px solid white
+      &:nth-last-child(1):nth-child(2n) // last child when it is even (right)
+        border-bottom: 0
+      &:nth-last-child(1):nth-child(2n+1), &:nth-last-child(2):nth-child(2n+1) // last child when it is odd and before last when it is odd (left)
+        border-bottom: 0
+ol
+  padding: 0
+  list-style-type: none
+  display: flex
+  flex-wrap: wrap
+  li
+    position: relative
+    margin: 1rem
+    background-color: #fcf451
+    padding: .5rem
+    border-radius: 4px
+    >span
+      &:first-child
+        position: absolute
+        right: 0
+        top: 0
+        transform: translate(50%, -50%)
+        background-color: #30bfa3
+        color: white
+        border-radius: 7px
+        text-align: center
+        max-width: 1.5rem
+        min-width: 1rem
+        padding: .1rem
 </style>
